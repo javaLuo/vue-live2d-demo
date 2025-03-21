0aed7fa4c2730a7cc58ddb243b989ca2c9b6d095
javaLuo <376693576@qq.com>
2025-03-21 23:38:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="liveCanvas" />
+  <canvas ref="liveCanvas" width="500" height="500"/>
 
   <div class="control">
     <button @click="expression('默认')">默认</button>
@@ -7,20 +7,15 @@
     <button @click="expression('忧伤')">忧伤</button>
     <button @click="expression('愤怒')">愤怒</button>
     <button @click="expression('吐舌')">吐舌</button>
-    <button @click="onVcodeShow">vcodeShow</button>
-    <button @click="onDialog">dialogShow</button>
   </div>
 
-  <Vcode :show="isVcodeShow" @close="onVcodeClose" :zIndex="2005" @success="onSuccess"/>
 </template>
 
 <script setup lang="ts">
-import { showDialog } from "vant";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import * as PIXI from "pixi.js";
 // @ts-ignore
 import { Live2DModel } from "pixi-live2d-display/cubism4";
-import Vcode from "vue3-puzzle-vcode";
 (window as any).PIXI = PIXI;
 
 let app: PIXI.Application;
@@ -36,9 +31,8 @@ onMounted(async () => {
     backgroundAlpha: 0,
   });
 
-  // model = await Live2DModel.from("./gougou_vts/狗狗.model3.json");
-  // model = await Live2DModel.from("./芙宁娜（vts）/芙宁娜.model3.json");
-  model = await Live2DModel.from("./jingying1.0/jingying.model3.json");
+  model = await Live2DModel.from("./gougou_vts/狗狗.model3.json");
+  // model = await Live2DModel.from("./jingying1.0/jingying.model3.json");
   app.stage.addChild(model);
   model.scale.set(0.1);
 });
@@ -52,25 +46,11 @@ function expression(type: string) {
   model.expression(type);
 }
 
-const isVcodeShow = ref(false);
-
-function onVcodeShow() {
-  isVcodeShow.value = true;
-}
-
-function onVcodeClose() {
-  isVcodeShow.value = false;
-}
-
-function onDialog() {
-  showDialog({
-    message: "提示",
-    closeOnClickOverlay: true,
-  });
-  isVcodeShow.value = true;
-}
-
-function onSuccess(a: number, b: any){
-  console.log(a, b);
-}
 </script>
+
+<style scoped>
+  canvas{
+    width: 500px;
+    height: 500px;
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { Dialog } from "vant";
-import "vant/lib/index.css";
-import "./styles/global.less";
 
 const app = createApp(App);
-app.use(Dialog);
 app.mount("#app");
